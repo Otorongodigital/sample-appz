bcee8a847368307542a764b50666c6326c7ce04d
Otorongodigital <otorongo.digital@gmail.com>
2022-11-29 12:58:59 +1100

    Update config.js

--- supervisor/src/utils/config.js
+++ supervisor/src/utils/config.js
@@ -1,5 +1,5 @@
 const config = {
-  client_id: "55ab76a60bda531add8663932344f994",
+  client_id: "0501da2177f7d0939a352eb980ca9fb6",
   server_url:
     "https://us-central1-livechat-experiments.cloudfunctions.net/restApi",
   account_url: "https://accounts.livechatinc.com/"
